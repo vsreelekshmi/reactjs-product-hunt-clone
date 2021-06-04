489f1e6b6d81c572c9faaa07dad579694f1b58eb
vsreelekshmi <sreelekshmi1771996@gmail.com>
2021-06-04 10:33:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reactjs-product-hunt-clone
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/reactjs-product-hunt-clone)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Cards from "./components/Cards";
+import Header from "./components/Header";
+// import Github from "./components/Github";
+
+const App = () => {
+	return (
+		<div>
+			<Header />
+			<Cards />
+		</div>
+	);
+};
+
+export default App;
